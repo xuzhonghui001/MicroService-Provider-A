8be4a450f0ebf2ed20602a5b8e93d082923cb38c
Zhonghui XU <zhonghui.xu.partner@decathlon.com>
2019-11-20 15:41:20 +0800

    init provide-a

---
+++ src/main/java/com/xzh/providea/ProvideAApplication.java
@@ -0,0 +1,13 @@
+package com.xzh.providea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProvideAApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProvideAApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/xzh/providea/ProvideAApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xzh.providea;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProvideAApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
